1d5d645df03b6463f870d0eb0f34b8304c03ab0b
Kamer Berat Topal <kamertopalxp@gmail.com>
2022-02-18 00:30:30 +0300

    Update and Delete ability added. Error messages edited.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
+    validate do |article|
+        errors.add :body, presence: true, length: { minimum:10 }, message: "char count is: #{article.body.size}"
+    end
 end
